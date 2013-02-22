07bb08971f17007d3547ef7e26949613f3890686
Dorian Snyder <dastin1015@gmail.com>
2013-02-22 11:55:18 -0800

    media: wrong if statement

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifndef ($(TARGET_QCOM_DISPLAY_VARIANT),legacy)
+ifneq ($(TARGET_QCOM_DISPLAY_VARIANT),legacy)
 ifneq ($(filter msm8960 msm8660 msm7x30,$(TARGET_BOARD_PLATFORM)),)
 
 include $(call all-subdir-makefiles)
