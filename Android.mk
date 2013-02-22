c42672501dc466badbb40a6aa32f077101c8c0ed
Dorian Snyder <dastin1015@gmail.com>
2013-02-22 10:43:27 -0800

    media: update ifndef for new flags

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
+ifndef ($(TARGET_QCOM_DISPLAY_VARIANT),legacy)
 ifneq ($(filter msm8960 msm8660 msm7x30,$(TARGET_BOARD_PLATFORM)),)
-ifeq ($(BOARD_USES_LEGACY_MEDIA),)
 
 include $(call all-subdir-makefiles)
 
